557a63ad1de3bcd4aa1c4f89e00144600e8f66e6
Kochetkov Danil <kochetkov214@gmail.com>
2019-10-12 14:33:45 +0700

    Добавляет модули и плагины в сборку, настраивает дев билд, настраивает дев сборку, добавляет точку входа и первый JSX файл

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
+    entry: './src/public/index.js',
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "index_bundle.js"
+    },
+    devServer: {
+        publicPath: "/",
+        contentBase: path.join(__dirname, "/dist"),
+        hot: true,
+        port: 9000,
+        overlay: true,
+        watchOptions: {
+            aggregateTimeout: 600,
+            poll: 1000,
+        },
+    },
     module: {
         rules: [
             {
@@ -7,7 +26,22 @@ module.exports = {
                 use: {
                     loader: "babel-loader"
                 }
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
             }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/public/index.html"
+        })
+    ],
+    resolve: {
+        extensions: [
+            '.js',
+            '.jsx'
         ]
     }
 };
