7a934ce541da9183d7b664cc8e86ea70b93e14e4
Kochetkov Danil <kochetkov214@gmail.com>
2019-10-13 09:48:46 +0700

    Подключает ESLint, подключает prettier, добавляет yarn.lock в gitignore

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,14 +1,9 @@
+import React from 'react';
 
-import React, { Component } from 'react';
-
-class App extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Hll wrld</h1>
-            </div>
-        );
-    }
+export default function App() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  );
 }
-
-export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,41 @@
 const path = require('path');
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/public/index.js',
-    output: {
-        path: path.join(__dirname, "/dist"),
-        filename: "index_bundle.js"
+  entry: './src/public/index.jsx',
+  output: {
+    path: path.join(__dirname, '/dist'),
+  },
+  devServer: {
+    publicPath: '/',
+    contentBase: path.join(__dirname, '/dist'),
+    hot: true,
+    port: 9000,
+    overlay: true,
+    watchOptions: {
+      aggregateTimeout: 600,
+      poll: 1000,
     },
-    devServer: {
-        publicPath: "/",
-        contentBase: path.join(__dirname, "/dist"),
-        hot: true,
-        port: 9000,
-        overlay: true,
-        watchOptions: {
-            aggregateTimeout: 600,
-            poll: 1000,
-        },
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/public/index.html"
-        })
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
-    resolve: {
-        extensions: [
-            '.js',
-            '.jsx'
-        ]
-    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/public/index.html',
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
 };
