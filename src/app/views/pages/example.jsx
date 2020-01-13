ef13b143ebba3cedea7964bab4b6eed5fa1eb11a
KochetkovDA <Kochetkov214@gmail.com>
2020-01-13 15:48:57 +0700

    Переименовывает роут и компонент с Test на examplePage

---
+++ src/app/views/pages/example.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const examplePage = () => (
+  <h2>
+     Hi, I am a example page!
+  </h2>
+);
+
+export default examplePage;
--- src/app/views/pages/test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Test = () => (
-  <h2>
-     Hi, I am a Test!
-  </h2>
-);
-
-export default Test;
